13e584e7225292b4e3288a0f89a3d535aa33e519
Toph3rZz <21510120@etu.unicaen.fr>
2016-03-17 16:52:51 +0100

    TP2

---
+++ fonction.php
@@ -0,0 +1,22 @@
+<?php
+function getHeader($startSession,$title)
+{
+    if ($startSession == true) {
+        session_start();
+
+        echo "
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset=\"UTF-8\">
+             <title>{$title}</title>
+                <link rel='stylesheet' href='./css/boostrap.min.css'>
+        </head>
+        <body>";
+    }
+}
+function getFooter(){
+    echo "
+        </body>
+        </html>";
+}
